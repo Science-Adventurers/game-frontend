7fb7b10768fbdbcb14dfddedf924300c04efd622
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-21 17:17:36 +0000

    Create start page (no CSS)
    - Add link to start page from home route.

--- game-frontend/src/index.js
+++ game-frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Start from './components/Start';
 import './index.css';
 import { Route, Router, browserHistory } from 'react-router';
 
 const Routes = (props) =>
   <Router {...props}>
     <Route path="/" component={ App }/>
+    <Route path="/start" component={ Start }/>
   </Router>
 
 ReactDOM.render(
