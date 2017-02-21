93958078d305b502c62de13d6d4392f27a9617ff
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-21 14:36:49 +0000

    Create first channel

---
+++ game-frontend/src/connection.js
@@ -0,0 +1,12 @@
+import { Socket } from "phoenix-js";
+
+let socket = new Socket("ws://smhack-game-api.herokuapp.com/socket", {name: "Super awesome team"})
+
+socket.connect()
+
+let channel = socket.channel("quiz", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+export default channel;
