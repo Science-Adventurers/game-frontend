f1187c7314f3771f9bff485e9c8ccc832c9aafc0
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-21 16:00:58 +0000

    Add first view

--- game-frontend/src/App.js
+++ game-frontend/src/App.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../public/assets/logo.png';
 import './App.css';
 import channel from './connection.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      adventure: ''
+    };
+  }
+  selectAdventure(adventure){
+     this.setState({adventure})
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <h2>Hi</h2>
+          <h2>Science Adventurers</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Pick your challenge!
         </p>
+        <ul className="challenge-list">
+          <li onClick={ () => this.selectAdventure("Space") }>
+            Space
+          </li>
+          <li onClick={ () => this.selectAdventure("Time") }>
+            Time
+          </li>
+          <li onClick={ () => this.selectAdventure("Energy") }>
+            Energy
+          </li>
+          <li onClick={ () => this.selectAdventure("Flight") }>
+            Flight
+          </li>
+          <li onClick={ () => this.selectAdventure("Medicine") }>
+            Medicine
+          </li>
+        </ul>
+        <label>Name:</label>
+        <input type="text" name="team-name" onChange={ (e) => this.setState({name: e.target.value})} value={ this.state.name }/>
+        <button>Start challenge</button>
       </div>
     );
   }
