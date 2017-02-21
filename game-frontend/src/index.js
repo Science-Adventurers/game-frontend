b83ba5767c5c5c656f334c3570c65b34eda14a8a
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-02-21 16:18:16 +0000

    Complete home page

--- game-frontend/src/index.js
+++ game-frontend/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Route, Router, browserHistory } from 'react-router';
+
+const Routes = (props) =>
+  <Router {...props}>
+    <Route path="/" component={ App }/>
+  </Router>
 
 ReactDOM.render(
-  <App />,
+  <Routes history={ browserHistory }/>,
   document.getElementById('root')
 );
