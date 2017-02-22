4159b178094a75fee767e7f3b5de1b14fa4b1ead
denesnori <denes.nori@yahoo.com>
2017-02-22 13:21:56 +0000

    make it secure

---
+++ game-frontend/src/connection.js
@@ -0,0 +1,10 @@
+import { Socket } from "phoenix-js";
+
+let socket = new Socket("wss://smhack-game-api.herokuapp.com/socket", {name: "Super awesome team"})
+
+socket.connect()
+
+let channel = socket.channel("quiz", {player_name:"Nori"})
+
+
+export default channel;
